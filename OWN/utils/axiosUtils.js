a6eea9c31b6c1174c0b13c2e1b89bce6b5d177cb
csbizz <95736373+csbizz@users.noreply.github.com>
2024-10-24 22:13:06 +0900

    Backup

--- OWN/utils/axiosUtils.js
+++ OWN/utils/axiosUtils.js
@@ -1,56 +1,55 @@
 import axios from 'axios';
 import isEmpty from './isEmpty.js';
+import HttpStatus from './HttpStatus.js';
 
 const HTTP_METHODS = Object.freeze({
-	GET: 'GET',
-	POST: 'POST',
-	PATCH: 'PATCH',
-	DELETE: 'DELETE',
-	PUT: 'PUT',
+  GET: 'GET',
+  POST: 'POST',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE',
+  PUT: 'PUT',
 });
 
-const BASE_URL = `http://localhost:3000`;
+async function axiosData({ base, url, method, data = {}, params = {} }) {
+  if (!url || !method) throw new Error('URL and Method is required');
+  if (!HTTP_METHODS[method]) throw new Error('Invalid HTTP method');
 
-const instance = axios.create({
-	baseURL: BASE_URL,
-	headers: {
-		'Content-Type': 'application/json',
-	},
-});
+  const instance = axios.create({
+    baseURL: base,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-async function axiosData({ url, method, data = {}, params = {} }) {
-	if (!url || !method) throw new Error('URL and Method is required');
-	if (!HTTP_METHODS[method]) throw new Error('Invalid HTTP method');
+  const response = await instance({ method, url, data, params })
+    .then(res => {
+      if (res.status === HttpStatus.NO_CONENT && isEmpty(res.data)) return res.status;
 
-	const response = await instance({ method, url, data, params })
-		.then(res => {
-			if (res.status === 204 && isEmpty(res.data)) return res.status;
+      return res.data;
+    })
+    .catch(e => {
+      return e;
+    });
 
-			return res.data;
-		})
-		.catch(e => {
-			throw e;
-		});
-
-	return response;
+  return response;
 }
 
-export async function axiosGet(url, params) {
-	return axiosData({ url, method: HTTP_METHODS.GET, params });
+export async function axiosGet(base, url, params) {
+  return axiosData({ base, url, method: HTTP_METHODS.GET, params });
 }
 
-export async function axiosPost(url, data) {
-	return axiosData({ url, method: HTTP_METHODS.POST, data });
+export async function axiosPost(base, url, data) {
+  return axiosData({ base, url, method: HTTP_METHODS.POST, data });
 }
 
-export async function axiosPatch(url, data) {
-	return axiosData({ url, method: HTTP_METHODS.PATCH, data });
+export async function axiosPatch(base, url, data) {
+  return axiosData({ base, url, method: HTTP_METHODS.PATCH, data });
 }
 
-export async function axiosPut(url, data) {
-	return axiosData({ url, method: HTTP_METHODS.PUT, data });
+export async function axiosPut(base, url, data) {
+  return axiosData({ base, url, method: HTTP_METHODS.PUT, data });
 }
 
-export async function axiosDelete(url) {
-	return axiosData({ url, method: HTTP_METHODS.DELETE });
+export async function axiosDelete(base, url, data) {
+  return axiosData({ base, url, method: HTTP_METHODS.DELETE, data });
 }
